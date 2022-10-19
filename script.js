b82575edb01efba746d8ad7aa1535ec467d3a527
StefanPacola <stevo.pacola@gmail.com>
2022-10-19 17:27:23 +0200

    finished desktop design and black bg when sidebar shows up

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const hamburger = document.querySelector(".icon-hamburger");
 const sideBar = document.querySelector(".sidebar");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
     if(sideBar.style.display === ("none")){
         sideBar.style.display = "block";
         hamburger.setAttribute("src", "./images/icon-close-menu.svg");
+        body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     }
     else{
         sideBar.style.display = "none";
         hamburger.setAttribute("src", "./images/icon-menu.svg");
+        body.style.backgroundColor = "";
     }
 })
